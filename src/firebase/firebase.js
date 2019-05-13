ae80d10298ee56ec19a301fd88c2c7db63906033
Carlos Romero <romerocm92@gmail.com>
2019-05-12 21:31:10 -0600

    Added google auth login/logout

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,6 @@
-import * as firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
 
 const config = {
     apiKey: "AIzaSyCgdYkGKbFynsQVYKPLt75AVI_5aY75hQ4",
@@ -13,8 +15,9 @@ const config = {
   firebase.initializeApp(config);
 
   const database = firebase.database();
+  const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
-  export { firebase, database as default };
+  export { firebase, googleAuthProvider, database as default };
 
 
 // database.ref().set({
